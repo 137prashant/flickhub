c01f0d5e58305f344a2835e2ba1989e833020889
137prashant <prashantsinghalpq@gmail.com>
2023-08-25 14:02:03 +0530

    error resolved 4

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import useFetch from "../../../hooks/useFetch";
 import { useSelector } from "react-redux";
 
-import Img from "../../../components/lazyLoadImage/Img"
+import Img from "../../../components/lazyLoadImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 function HeroBanner() {
