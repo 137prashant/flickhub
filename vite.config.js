64461da4723ecd8f6c48074cb34ecbd790c3f495
137prashant <prashantsinghalpq@gmail.com>
2023-09-17 23:27:39 +0530

    updated13

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './index.html', // Specify the path to your index.html
-    },
-  },
+  plugins: [react()]
 })
